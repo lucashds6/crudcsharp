eca0165f3724a223c0c731dca5ba41ddc3dfd799
LUCAS HENRIQUE DA SILVA <lucas.hsilva50@senacsp.edu.br>
2023-07-19 17:18:37 -0300

    first commit

---
+++ WFGravarDadosMySQL/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WFGravarDadosMySQL
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection Conexao;
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string data_source = "datasource=localhost;username=root;password=;database=db_agenda";
+                Conexao = new MySqlConnection(data_source);
+
+                string sql = "INSERT INTO contato (nome,email,telefone)"+"VALUES"+"('"+txtNome.Text+"','"+txtEmail.Text+"', '"+txtTelefone.Text+"')";
+                MySqlCommand comando = new MySqlCommand(sql, Conexao);
+                Conexao.Open();
+                comando.ExecuteReader();
+                MessageBox.Show("Cadastro inserido com sucesso!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        private void txt_Buscar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
